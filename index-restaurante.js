11f70b06d7b8e0d6d83c1d53a79760efdb5d193c
Eduardo <91166485+dudu9533@users.noreply.github.com>
2022-01-06 01:25:01 -0300

    Update index-restaurante.js

--- index-restaurante.js
+++ index-restaurante.js
@@ -1,20 +1,3 @@
-/* 
-Pensar e fazer um projeto  de um sistema 
-que a pessoa pede uma comida e 
-aparece para a cozinha o seu  pedido
-*/
-
-/*
-Ideias:
-
-- Restaurante colocar para o cliente o cardápio no console
-- Pessoa colocar seu nome
-- Pessoa colocar o número da mesa
-- Pessoa fazer seu pedido
-- Pessoa pede em sistema de nome do prato, que para a cozinha aparece 
-tudo que há no prato e observações
-*/ 
-
 import { CardapioRestaurante } from "./Cardápio/Cardápio-restaurante.js";
 import { ClienteRestaurante } from "./Clientes/Cliente-restaurante.js";
 
